c5fca2b560c363265dd9f8f5bb3d7383c19589d6
Alamuru Sri Sai Ganesh Reddy <ganesh.mango94@gmail.com>
2025-05-22 17:51:32 +0530

    refactor and make tcpKeepAlive true

--- src/main/java/com/gan/authservice/configuration/RedisConfig.java
+++ src/main/java/com/gan/authservice/configuration/RedisConfig.java
@@ -12,4 +12,5 @@ public class RedisConfig {
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         return new StringRedisTemplate(redisConnectionFactory);
     }
+
 }
